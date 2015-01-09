775916023decbed977a53e147055bf5db5a3a280
Sorel <sodeve619@gmail.com>
2015-01-09 10:24:19 -0500

    Fix log out problem with a wrong render in a view, add authentication request to see projects and dashboard and fix a problem with the edit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,12 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   respond_to :html
 
   def index
     @projects = Project.all
-    @project = Project.new
+    @project = current_user.projects.new
 
     render 'index'
     #respond_with(@projects)
@@ -21,10 +22,12 @@ class ProjectsController < ApplicationController
   #end
 
   def edit
+    @project = Project.find(params[:id])
+    render :edit
   end
 
   def create
-    @project = Project.new(project_params)
+    @project = current_user.projects.new(project_params)
     @project.save
 
     if @project.save
@@ -36,7 +39,13 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    @project.update(project_params)
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
     #respond_with(@project)
   end
 
